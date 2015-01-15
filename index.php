2490a6218ac7a335ae6789fe6457aef050d6e43a
Mr.Raindrop <tekkahs@gmail.com>
2015-01-15 16:59:06 +0800

    mod style

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 <div class="col-mb-12 col-9" id="main" role="main">
 	<?php while($this->next()): ?>
         <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
+        	<div class="post-finger"><?php $this->category(','); ?></div>
 			<h2 class="post-title" itemprop="name headline"><a itemtype="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
 			<ul class="post-meta">
 				<li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
@@ -23,7 +24,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 				<li itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
 			</ul>
             <div class="post-content" itemprop="articleBody">
-    			<?php $this->content('- 阅读剩余部分 -'); ?>
+    			<?php $this->content('更多>>'); ?>
             </div>
         </article>
 	<?php endwhile; ?>
--- post.php
+++ post.php
@@ -3,6 +3,7 @@
 
 <div class="col-mb-12 col-8" id="main" role="main">
     <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
+        <div class="post-finger"><?php $this->category(','); ?></div>
         <h1 class="post-title" itemprop="name headline"><a itemtype="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h1>
         <ul class="post-meta">
             <li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
